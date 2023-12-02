9dc8420bbca0c5a69ac819cadb0c78e4b0c0a163
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2023-12-02 11:18:20 +0100

    Fixed the interractions with the pass button. Implemented so that you will never know what card that got passed into the deck. Added a message when you defend against jack. Fixed a function that disables all three buttons at the same time. removed unused code that was for other intentions at the start of the project

--- app/src/main/java/com/example/examinationcardgame/GameActivity.kt
+++ app/src/main/java/com/example/examinationcardgame/GameActivity.kt
@@ -1,12 +1,10 @@
 package com.example.examinationcardgame
 
-import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
-import android.util.Log
-import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
 
@@ -16,48 +14,88 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
+
+        val passButton: ImageView = findViewById(R.id.passButton)
         val playButton: ImageView = findViewById(R.id.playButton)
-        val allCards = Card.values().toList()
-        val allFragments = mutableListOf<CardFragment>()
-        val backCardFragment = CardFragment()
-        backCardFragment.initCard(Card.CARD_BACK)
+        val defendButton: ImageView = findViewById(R.id.defendButton)
+        var isPassButtonClicked = false
+        val cardBackgroundFragment = CardFragment()
+        cardBackgroundFragment.initCard(Card.CARD_BACKGROUND)
+        val allCards = Card.values().toMutableList()
+        val playableCards = allCards.filter{ it != Card.CARD_BACKGROUND &&
+                it != Card.CLUBS_JACKSAVED && it != Card.CLUBS_JACKSURVIVE }.shuffled().toMutableList()
 
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.deckContainer, backCardFragment)
+        transaction.add(R.id.deckContainer, cardBackgroundFragment)
         transaction.commit()
 
         val firstFragment = CardFragment()
+        firstFragment.initCard(Card.CARD_BACKGROUND)
 
-        firstFragment.initCard(Card.CARD_BACK)
 
-
-        for (i in allCards.indices) {
-            val fragment = CardFragment()
-            fragment.initCard(Card.CARD_BACK)
-            allFragments.add(fragment)
+        fun inactivateButtons() {
+            passButton.isEnabled = false
+            playButton.isEnabled = false
+            defendButton.isEnabled = false
         }
 
-
-        val cardsWithoutBack = allCards.filter{ it != Card.CARD_BACK &&
-                it != Card.CLUBS_JACKSAVED && it != Card.CLUBS_JACKSURVIVE }.shuffled()
-
-        playButton.setOnClickListener {
-            if (currentCardIndex < cardsWithoutBack.size) {
-                val card = cardsWithoutBack[currentCardIndex]
+        passButton.setOnClickListener {
+            if (currentCardIndex < playableCards.size) {
+                isPassButtonClicked = true
+                val card = playableCards[currentCardIndex]
+                playableCards.removeAt(currentCardIndex)
+                val RandomIndex = Random.nextInt(playableCards.size - currentCardIndex) + currentCardIndex
+                playableCards.add(RandomIndex, card)
 
                 val fragment = CardFragment()
-                fragment.initCard(card)
+                fragment.initCard(Card.CARD_BACKGROUND, isPassButtonClicked)
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.deckContainer, fragment)
+                transaction.commit()
+            }
+        }
 
+        playButton.setOnClickListener {
+            if (currentCardIndex < playableCards.size) {
+                isPassButtonClicked = false
+                val card = playableCards[currentCardIndex]
+                val fragment = CardFragment()
+                fragment.initCard(card, isPassButtonClicked)
                 val transaction = supportFragmentManager.beginTransaction()
                 transaction.replace(R.id.deckContainer, fragment)
                 transaction.commit()
 
                 if (card == Card.CLUBS_JACKDEATH) {
-                    Toast.makeText(this, "Ahw shit, Jack got you. You lost the game", Toast.LENGTH_LONG).show()
-                    playButton.isEnabled = false
+                    Toast.makeText(this, "Jack caught you off guard. RIP (Ripped In Pieces)",
+                        Toast.LENGTH_LONG).show()
+                    inactivateButtons()
                 }
                 currentCardIndex++
             }
         }
+
+        defendButton.setOnClickListener {
+            if (currentCardIndex < playableCards.size) {
+                isPassButtonClicked = false
+                var card = playableCards[currentCardIndex]
+                if (card == Card.CLUBS_JACKDEATH) {
+                    card = Card.CLUBS_JACKSURVIVE
+                    playableCards[currentCardIndex] = card
+                }
+                val fragment = CardFragment()
+                fragment.initCard(card, isPassButtonClicked)
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.deckContainer, fragment)
+                transaction.commit()
+
+                if (card == Card.CLUBS_JACKSURVIVE) {
+                    Toast.makeText(this,
+                        "Jack is now unconscious and you survived. Congratulations!",
+                        Toast.LENGTH_LONG).show()
+                    inactivateButtons()
+                }
+                currentCardIndex++
+            }
+        }
+        }
     }
-}
